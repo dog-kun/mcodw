404ef2dede7e60d59f3f1dbc16be1f8117dde570
dog-kun <141000609+dog-kun@users.noreply.github.com>
2025-05-18 13:48:03 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,12 +7,19 @@ import { head, nav, sidebar } from './configs'
 const APP_BASE_PATH = basename(process.env.GITHUB_REPOSITORY || '')
 
 export default defineConfig({
+   themeConfig: {
+    /* 访客统计 */
+    visitor: {
+      /** 统计 id（单独页面的统计会作为前缀使用）*/
+      badgeId: 'maomao1996.vitepress-nav-template',
+    },
+  },
   outDir: '../dist',
   base: APP_BASE_PATH ? `/${APP_BASE_PATH}/` : '/',
 
   lang: 'zh-CN',
-  title: '茂茂物语',
-  description: '茂茂的成长之路，包含前端常用知识、源码阅读笔记、各种奇淫技巧、日常提效工具等',
+  title: 'MCODW',
+  description: '一个粘液生电玩法的我的世界服务器',
   head,
 
   lastUpdated: true,
